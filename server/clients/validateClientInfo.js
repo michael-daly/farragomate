65a3248370e9e12febb7e243d44500c3a56ae919
Michael <mjdaly10@gmail.com>
2020-07-04 16:09:19 -0700

    Added validateClientInfo()

---
+++ server/clients/validateClientInfo.js
@@ -0,0 +1,31 @@
+const fieldData      = require ('$/clients/fieldData.js');
+const validateFields = require ('$/fields/validateFields.js');
+
+const { hasClientName } = require ('$/clients/GameClientNames.js');
+
+const { FIELD_ERR_NONE, FIELD_ERR_UNIQUE } = require ('$/fields/constants.js');
+
+
+/**
+ * @param   {Object} info
+ * @returns {Integer|Array} [fieldName, errorCode] or FIELD_ERR_NONE if no error.
+ */
+const validateClientInfo = info =>
+{
+	const result = validateFields (info, fieldData);
+
+	if ( result !== FIELD_ERR_NONE )
+	{
+		return result;
+	}
+
+	if ( hasClientName (info.displayName) )
+	{
+		return ['displayName', FIELD_ERR_UNIQUE];
+	}
+
+	return FIELD_ERR_NONE;
+};
+
+
+module.exports = validateClientInfo;
--- server/packets/handlers/RegisterInfo.js
+++ server/packets/handlers/RegisterInfo.js
@@ -1,40 +1,30 @@
-const fieldData      = require ('$/clients/fieldData.js');
-const sanitizeFields = require ('$/fields/sanitizeFields.js');
-const validateFields = require ('$/fields/validateFields.js');
+const fieldData          = require ('$/clients/fieldData.js');
+const sanitizeFields     = require ('$/fields/sanitizeFields.js');
+const validateClientInfo = require ('$/clients/validateClientInfo.js');
 
 const { RequestPacket } = require ('~/packets/types.js').packetTypes;
 const { RegisterInfo }  = require ('~/packets/commands.js').packetCommands;
 
 const { addPacketHandler } = require ('$/packets/PacketHandlers.js');
+const { addClientName }    = require ('$/clients/GameClientNames.js');
 
-const { addClientName, hasClientName } = require ('$/clients/GameClientNames.js');
-
-const { FIELD_ERR_NONE, FIELD_ERR_UNIQUE } = require ('$/fields/constants.js');
+const { FIELD_ERR_NONE } = require ('$/fields/constants.js');
 
 
 addPacketHandler (RequestPacket, RegisterInfo, ( client, packet ) =>
 {
-	const { body } = packet;
-
-	const info   = sanitizeFields (body, fieldData);
-	const result = validateFields (body, info);
+	const info   = sanitizeFields (packet.body, fieldData);
+	const result = validateClientInfo (info);
 
 	if ( result !== FIELD_ERR_NONE )
 	{
 		client.sendPacket ('Reject', packet, result);
-		return;
 	}
-
-	const { displayName } = info;
-
-	if ( hasClientName (displayName) )
+	else
 	{
-		client.sendPacket ('Reject', packet, ['displayName', FIELD_ERR_UNIQUE]);
-		return;
+		addClientName (info.displayName);
+		client.setInfoField ('displayName', info.displayName);
+
+		client.sendPacket ('Accept', packet);
 	}
-
-	client.info.displayName = displayName;
-	addClientName (displayName);
-
-	client.sendPacket ('Accept', packet);
 });
