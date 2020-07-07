1abe440bda82fcf67c915f7333634b62b8f9455f
Michael <mjdaly10@gmail.com>
2020-07-06 22:37:59 -0700

    Added config for the API request

---
+++ server/config/apiRequest.js
@@ -0,0 +1,16 @@
+module.exports =
+{
+	baseURL: 'https://api.wordnik.com/v4/words.json/randomWords',
+
+	hasDictionaryDef:   true,
+	minDictionaryCount: 1,
+	maxDictionaryCount: -1,
+
+	minCorpusCount: 10000,
+	maxCorpusCount: -1,
+
+	minLength: 3,
+	maxLength: 20,
+
+	limit: 16,
+};
