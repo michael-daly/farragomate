28e8be52bc69a0e5dd89247ee72feb33b2e08e9f
Michael <mjdaly10@gmail.com>
2020-07-08 18:47:50 -0700

    Moved TitleLogo component to misc/ folder

--- client/js/misc/TitleLogo.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-
-const TitleLogo = () => <div className='title-logo'><img src='./img/titleLogo.png' /></div>;
-
-
-export default TitleLogo;
---
+++ client/js/ui/TitleLogo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const TitleLogo = () =>
+(
+	<div className='title-logo center-content'><img src='./img/titleLogo.png' /></div>
+);
+
+
+export default TitleLogo;
