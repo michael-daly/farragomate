a011759b96c0bfee424728922035c1bf8884ceaf
Michael <mjdaly10@gmail.com>
2020-07-19 16:28:20 -0700

    Rearranged UIField rendering method + added onChange prop

--- client/js/ui/UIFields.jsx
+++ client/js/ui/UIFields.jsx
@@ -7,51 +7,71 @@ import UITextbox  from '#/ui/UITextbox.jsx';
 
 class UIFields extends Component
 {
+	constructor ( props )
+	{
+		super (props);
+		this.state = {};
+	}
+
 	render ()
 	{
-		const { props }     = this;
-		const { fieldData } = props;
+		const { fieldData, onChange = () => {} } = this.props;
 
-		const elementArr = [];
-
-		for ( let fieldName in fieldData )
-		{
-			const { type, min, max, defaultValue, label } = fieldData[fieldName];
-
-			let control;
-
-			if ( type === 'string' )
+		const fields =
+		(
+			<div className='center-content fields-container'>
 			{
-				control = <UITextbox maxLength={max} value={defaultValue} />;
-			}
-			else
-			{
-				const options = [];
-
-				let selection = { label: defaultValue, value: defaultValue };
-
-				for ( let i = min; i < max; i++ )
+				Object.keys (fieldData).map (( fieldName, index ) =>
 				{
-					options.push ({ label: i, value: i });
-				}
+					const { type, min, max, defaultValue, label } = fieldData[fieldName];
 
-				control = <UIDropdown options={options} value={selection} />;
+					const onControlChange = event => onChange (event, fieldName, fieldData);
+
+					let control;
+
+					if ( type === 'string' )
+					{
+						control = <UITextbox
+							maxLength={max}
+							value={defaultValue}
+							onChange={onControlChange}
+						/>;
+					}
+					else
+					{
+						const options = [];
+
+						let selection = { label: defaultValue, value: defaultValue };
+
+						for ( let i = min; i <= max; i++ )
+						{
+							options.push ({ label: i, value: i });
+						}
+
+						control = <UIDropdown
+							options={options}
+							value={selection}
+							onChange={onControlChange}
+						/>;
+					}
+
+					const element =
+					(
+						<div className='field' key={`${index} ${fieldName} ${min} ${max} ${label}`}>
+							<span style={{ display: 'inline' }}>
+								<div className='field-label'>{label}:</div>
+								{control}
+							</span>
+						</div>
+					);
+
+					return element;
+				})
 			}
+			</div>
+		);
 
-			const element =
-			(
-				<div className='field'>
-					<span style={{ display: 'inline' }}>
-						<div className='field-label'>{label}:</div>
-						{control}
-					</span>
-				</div>
-			);
-
-			elementArr.push (element);
-		}
-
-		return <div className='center-content fields-container'>{elementArr.map (el => el)}</div>;
+		return fields;
 	}	
 }
 
