f8f18322f236a75d03be72e8415e8078af6779cd
Michael <mjdaly10@gmail.com>
2020-08-09 20:36:07 -0700

    Added preliminary registration info sending

--- client/js/Register/Register.jsx
+++ client/js/Register/Register.jsx
@@ -8,12 +8,39 @@ import UIButton  from '#/ui/UIButton.jsx';
 
 import fieldData from '~/clients/fieldData.js';
 
+import { sendRequestPacket } from '#/socket/actions.js';
+
 
 class Register extends Component
 {
 	constructor ( props )
 	{
 		super (props);
+
+		this.state =
+		{
+			enablePlayBtn: false,
+			sentInfo:      false,
+
+			fields: {},
+		};
+	}
+
+	onFieldChange ( field, value, fieldName, numRequired )
+	{
+		if ( this.state.sentInfo )
+		{
+			return;
+		}
+
+		this.setState ({ enablePlayBtn: numRequired <= 0, fields: { [fieldName]: value } });
+	}
+
+	onClickPlay ( event )
+	{
+		this.setState ({ enablePlayBtn: false, sentInfo: true });
+
+		this.props.sendRegisterInfo (this.state.fields);
 	}
 
 	render ()
@@ -23,8 +50,20 @@ class Register extends Component
 				<div>
 					<TitleLogo />
 
-					<UIFields className='button-menu' fieldData={fieldData} />
-					<UIButton type='magnet' inline={true} text='Play >>' />
+					<UIFields
+						className='button-menu'
+						fieldData={fieldData}
+						disabled={this.state.sentInfo}
+						onChange={this.onFieldChange.bind (this)}
+					/>
+
+					<UIButton
+						type='magnet'
+						inline={true}
+						text='Play >>'
+						onClick={this.onClickPlay.bind (this)}
+						disabled={!this.state.enablePlayBtn}
+					/>
 				</div>
 			</div>
 		);
@@ -39,7 +78,15 @@ const mapStateToProps = state =>
 
 const mapDispatchToProps = dispatch =>
 {
-	return {};
+	const props =
+	{
+		sendRegisterInfo ( info )
+		{
+			dispatch (sendRequestPacket ('RegisterInfo', info));
+		},
+	};
+
+	return props;
 };
 
 
