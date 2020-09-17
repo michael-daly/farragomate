9a9ccad8ee9765a676663333814cfb6d0405ab0d
Michael <mjdaly10@gmail.com>
2020-09-17 08:52:24 -0700

    Added name permutations to prevent impersonation

--- server/clients/GameClientNames.js
+++ server/clients/GameClientNames.js
@@ -2,11 +2,19 @@ const GameClientNames = new Set ();
 
 
 /**
+ * Several permutations, such as all lowercase and different characters that look similar,
+ * need to be checked here to be thorough.
+ */
+
+
+/**
  * @param {string} displayName
  */
 const addClientName = displayName =>
 {
 	GameClientNames.add (displayName);
+	GameClientNames.add (displayName.toLowerCase ());
+	GameClientNames.add (displayName.toLowerCase ().replace (/0/g, 'o').replace (/i/ig, 'l'));
 };
 
 /**
@@ -15,6 +23,8 @@ const addClientName = displayName =>
 const removeClientName = displayName =>
 {
 	GameClientNames.delete (displayName);
+	GameClientNames.delete (displayName.toLowerCase ());
+	GameClientNames.delete (displayName.toLowerCase ().replace (/0/g, 'o').replace (/i/ig, 'l'));
 };
 
 /**
@@ -23,7 +33,8 @@ const removeClientName = displayName =>
  */
 const hasClientName = displayName =>
 {
-	return GameClientNames.has (displayName);
+	return GameClientNames.has (displayName) || GameClientNames.has (displayName.toLowerCase ()) ||
+	       GameClientNames.has (displayName.toLowerCase ().replace (/0/g, 'o').replace (/i/ig, 'l'));
 };
 
 
